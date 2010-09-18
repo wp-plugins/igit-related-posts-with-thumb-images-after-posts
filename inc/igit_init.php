9714145cc0020d5bdff0d2866ae70d9134d04eb7
phpaddicted <phpaddicted@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-18 06:28:25 +0000

    tagging version 3.5
    git-svn-id: https://plugins.svn.wordpress.org/igit-related-posts-with-thumb-images-after-posts/tags/3.5@290857 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/igit_init.php
+++ inc/igit_init.php
@@ -1,6 +1,7 @@
 <?php
 
 $igit_rpwt = array( "text_show" => "<h4>Related Posts :</h4>",
+					"no_related_post_text" => "No related posts found.",
 					"auto_show" => "1",
 					"related_post_num" => "4",
 					"display_thumb" => "1",
@@ -16,4 +17,24 @@ $igit_rpwt = array( "text_show" => "<h4>Related Posts :</h4>",
 					"fonts_size" => "12",
 					"fonts_color" => "000000",
 					"img_border_color" => "DDDDDD");
+
+$igit_rpwt_default = array( "text_show" => "<h4>Related Posts :</h4>",
+					"no_related_post_text" => "No related posts found.",
+					"auto_show" => "1",
+					"related_post_num" => "4",
+					"display_thumb" => "1",
+					"display_title" => "1",
+					"thumb_width" => "100",
+					"thumb_height" => "100",
+					"related_post_style" => "1",
+					"igit_credit" => "1",
+					"bk_color" => "FFFFFF",
+					"bk_hover_color" => "DDDDDD",
+					"bk_hover_color" => "DDDDDD",
+					"fonts_family" => "Geneva",
+					"fonts_size" => "12",
+					"fonts_color" => "000000",
+					"img_border_color" => "DDDDDD");
+
+				
 ?>
